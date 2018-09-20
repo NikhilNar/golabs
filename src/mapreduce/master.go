6ea79f25755fe55ef995147f82a21dedf857efec
nikhilnar <nik.nar123@gmail.com>
2018-09-19 22:49:42 -0400

    Part 2 implementation completed

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -3,12 +3,10 @@ package mapreduce
 import "container/list"
 import "fmt"
 
-
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -30,5 +28,34 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	myWorkers := make(map[string]*WorkerInfo)
+	mr.Workers = myWorkers
+	totalMapJobs := mr.nMap
+	totalReduceJobs := mr.nReduce
+	for i := 0; i < 2; i++ {
+		worker := <-mr.registerChannel
+		mr.Workers[worker] = &WorkerInfo{worker}
+		for ; totalMapJobs > 0; totalMapJobs-- {
+			args := &DoJobArgs{File: mr.file, Operation: Map, JobNumber: mr.nMap - totalMapJobs, NumOtherPhase: mr.nReduce}
+			var reply DoJobReply
+			ok := call(worker, "Worker.DoJob", args, &reply)
+			if ok == false {
+				DPrintf("Something wrong with RPC===============\n")
+			}
+
+		}
+
+		for ; totalReduceJobs > 0; totalReduceJobs-- {
+			args := &DoJobArgs{File: mr.file, Operation: Reduce, JobNumber: mr.nReduce - totalReduceJobs, NumOtherPhase: mr.nMap}
+			var reply DoJobReply
+			ok := call(worker, "Worker.DoJob", args, &reply)
+			if ok == false {
+				DPrintf("Something wrong with RPC===============\n")
+			}
+
+		}
+
+	}
+
 	return mr.KillWorkers()
 }
